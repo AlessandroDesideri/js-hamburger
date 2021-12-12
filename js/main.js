60f974971b609110de6945bda07601dc7f3d3eaf
Alessandro <alessandro01desideri@gmail.com>
2021-12-12 18:47:00 +0100

    Inizializzazione Variabili

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 // Inizializzo le variabili
 //  Ham Menu
+const hamburger = document.querySelector('.hamburger-menu');
 // Apertura ham menu
+const button = document.querySelector('.fa-bars');
 // Chiusura ham menu
+const closeButton = document.querySelector('.close');
 
 // Funzione Apertura ham menu
 // Funzione Chiusura ham menu
